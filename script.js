05a1d42cc65e171060ce9e115306137cb13b093f
answjddnjs04 <infinitefoever@naver.com>
2025-09-08 09:16:08 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function gameLoop() {
-    console.log('Game loop started');
-    clearCanvas();
-    drawSquare();
-    applyGravity();
-    moveSquare();
-    requestAnimationFrame(gameLoop);
-    console.log('Game loop ended');
-}
